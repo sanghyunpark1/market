9f9cca9d98f8b6f8894be1880b7a075fd09431dd
sanghyunpark1 <adsry8768@naver.com>
2021-04-09 11:25:42 +0900

    prodct-market

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import MainPageComponent from "./main";
+import { Switch, Route } from "react-router-dom";
+import ProductPage from "./product";
+import UploadPage from "./upload";
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path="/">
+          <MainPageComponent />
+        </Route>
+        <Route exact={true} path="/products/:id">
+          <ProductPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
